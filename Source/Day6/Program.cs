9ead9fae43f7363fe9e26a49471d711252f52251
henrik-e-standal <henrik.standal@protonmail.com>
2025-12-08 16:26:52 +0100

    Add solution for day6 puzzles. (#15)
    Added solution for day 6.
    
    ---------
    
    Co-authored-by: Henrik <henrik.standal@nxtech.no>

---
+++ Source/Day6/Program.cs
@@ -0,0 +1,64 @@
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Aoc25.Common;
+
+namespace Aoc25.Day6
+{
+    public class Program
+    {
+        /// <summary>
+        /// Gets the input to use for the puzzle.
+        /// </summary>
+        /// <remarks>
+        /// See solution readme for documentation on program arguments.
+        /// </remarks>
+        /// <param name="args"> Arguments passed via command line. </param>
+        /// <returns> The input to use for the puzzle. </returns>
+        private static bool TryGetPuzzleInput(string[] programArgs, [NotNullWhen(true)] out string? puzzleInput)
+        {
+            bool dryRunRequested = false;
+
+            if(programArgs.Length > 1) {
+                dryRunRequested = string.Equals(programArgs[1], "1");
+            }
+            if(programArgs.Length > 0) {
+                puzzleInput = File.ReadAllText(programArgs[0]);
+            }
+            else {
+#if DEBUG
+                puzzleInput = Day6A.PuzzleInput.Personalized;
+#else
+                puzzleInput = null;
+#endif
+            }
+
+            if(dryRunRequested) {
+                puzzleInput = null;
+            }
+
+            return (!dryRunRequested && (puzzleInput != null));
+        }
+
+        /// <summary>
+        /// Entry point for the program.
+        /// </summary>
+        /// <param name="args"> Passed arguments. </param>
+        public static void Main(string[] args)
+        {
+            ulong puzzleResultPartA = 0;
+            ulong puzzleResultPartB = 0;
+
+            if(TryGetPuzzleInput(args, out string? puzzleInput))
+            {
+                BenchmarkTimer.Tick();
+                puzzleResultPartA = Day6A.PuzzleSolver.Solve(puzzleInput);
+                puzzleResultPartB = Day6B.PuzzleSolver.Solve(puzzleInput);
+                BenchmarkTimer.Tock();
+                BenchmarkTimer.PrintElapsedMilliseconds();
+            }
+
+            FastConsole.WriteLine($"{puzzleResultPartA} {puzzleResultPartB}");     
+        }
+    }
+}
