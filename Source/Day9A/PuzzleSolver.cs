dccec2761eada6904b72e88854247136ad1cd13d
Henrik <henrik.standal@nxtech.no>
2025-12-09 22:25:41 +0100

    wip

---
+++ Source/Day9A/PuzzleSolver.cs
@@ -0,0 +1,38 @@
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Aoc25.Common;
+
+namespace Aoc25.Day9A
+{
+    /// <summary>
+    /// Contains the puzzle solving code.
+    /// </summary>
+    public static class PuzzleSolver
+    { 
+        /// <summary>
+        /// Attempts to get the numeric number of a character.
+        /// </summary>
+        /// <param name="character"> The character whose numeric value to get. </param>
+        /// <param name="numericValue"> The numeric value of the character. </param>
+        /// <returns> True if the character was a number, otherwise false. </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool TryGetCharacterNumericValue(char character, out uint numericValue)
+        {
+            numericValue = (uint)(character - '0');
+            return (numericValue <= 9);
+        }
+
+        private 
+
+        /// <summary>
+        /// Solve the puzzle using the passed puzzle input.
+        /// </summary>
+        /// <param name="puzzleInput"> The puzzle input to solve for. </param>
+        /// <returns> The solution to the puzzle, solving for the passed puzzle input. </returns>
+        public static ulong Solve(string puzzleInput)
+        {
+            return 0;
+        }
+    }
+}
